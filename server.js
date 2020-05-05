993b494778bac51908a27881b13b2bc87cbb1a2f
soaicccca <minhthienpham0611@gmail.com>
2020-05-05 21:22:38 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const compression = require('compression');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const chalk = require('chalk');
+const errorHandler = require('errorhandler');
+const dotenv = require('dotenv');
+const MongoStore = require('connect-mongo')(session);
+const flash = require('express-flash');
+const path = require('path');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const expressStatusMonitor = require('express-status-monitor');
+const sass = require('node-sass-middleware');
+const listEndpoints = require('express-list-endpoints');
+const multer = require('multer');
+
+
+const app = express();
+dotenv.config();
+
+
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect(process.env.MONGODB_URI);
+mongoose.connection.on('error', (err) => {
+    console.error(err);
+    console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
+    process.exit();
+});
+
+app.set('host', process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0');
+app.set('port', process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(expressStatusMonitor());
+app.use(compression());
+
+app.use(sass({
+    src: path.join(__dirname, 'public'),
+    dest: path.join(__dirname, 'public')
+}));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+    resave: true,
+    saveUninitialized: true,
+    secret: process.env.SESSION_SECRET,
+    cookie: { maxAge: 1209600000 }, // two weeks in milliseconds
+    store: new MongoStore({
+        url: process.env.MONGODB_URI,
+        autoReconnect: true,
+    })
+}));
+
+app.use('/', express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
+app.get('/', (req, res) => {
+    res.status(200).send('<h1>hello</h1>')
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+if (process.env.NODE_ENV === 'development') {
+    // only use in development
+    app.use(errorHandler());
+} else {
+    app.use((err, req, res, next) => {
+        console.error(err);
+        res.status(500).send('Server Error');
+    });
+}
+
+app.listen(app.get('port'), () => {
+    console.log('%s App is running at http://localhost:%d in %s mode', chalk.green('✓'), app.get('port'), app.get('env'));
+    console.log(listEndpoints(app));
+    console.log('  Press CTRL-C to stop\n');
+});
+
+module.exports = app;
